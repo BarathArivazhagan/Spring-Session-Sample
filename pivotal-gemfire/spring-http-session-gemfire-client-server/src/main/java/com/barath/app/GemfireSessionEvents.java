694b1de84c2e84182d5f2c839deb0d169555a964
BarathArivazhagan <barathmacdec91@gmail.com>
2018-11-18 12:26:31 +0530

    reorganising project structure to accomodate based on store implementations

---
+++ pivotal-gemfire/spring-http-session-gemfire-client-server/src/main/java/com/barath/app/GemfireSessionEvents.java
@@ -0,0 +1,40 @@
+package com.barath.app;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.session.events.SessionCreatedEvent;
+import org.springframework.session.events.SessionDeletedEvent;
+import org.springframework.session.events.SessionDestroyedEvent;
+import org.springframework.session.events.SessionExpiredEvent;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class GemfireSessionEvents {
+	
+	
+	@EventListener
+	public void handleSessionCreationEvent(SessionCreatedEvent event){
+		System.out.println("SESSION CREATED EVENT "+event.getSession().getId());
+		
+	}
+	
+	@EventListener
+	public void handleSessionDeletionEvent(SessionDeletedEvent event){
+		System.out.println("SESSION Deleted EVENT "+event.getSession().getId());
+		
+	}
+	
+	@EventListener
+	public void handleSessionDestroyedEvent(SessionDestroyedEvent event){
+		System.out.println("SESSION Destroyed EVENT "+event.getSession().getId());
+		
+	}
+
+	
+	@EventListener
+	public void handleSessionExpiredEvent(SessionExpiredEvent event){
+		System.out.println("SESSION Expired EVENT "+event.getSession().getId());
+		
+	}
+
+}
